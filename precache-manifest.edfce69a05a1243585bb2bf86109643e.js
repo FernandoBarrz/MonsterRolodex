d76545879ef1cff37e15221a310c9eab4adf98a4
FernandoBarrz <barrioslui02@gmail.com>
2020-01-20 21:37:59 -0600

    Updates

---
+++ precache-manifest.edfce69a05a1243585bb2bf86109643e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b295a1dc46ca76b7b4c9b7160baba914",
+    "url": "/MonsterRolodex/index.html"
+  },
+  {
+    "revision": "fd602d83c2fa8b83dce6",
+    "url": "/MonsterRolodex/static/css/main.3341b8c4.chunk.css"
+  },
+  {
+    "revision": "b38ff15910eb84936896",
+    "url": "/MonsterRolodex/static/js/2.156a9e66.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/MonsterRolodex/static/js/2.156a9e66.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fd602d83c2fa8b83dce6",
+    "url": "/MonsterRolodex/static/js/main.84409f03.chunk.js"
+  },
+  {
+    "revision": "6f4b2eeec65abacd6453",
+    "url": "/MonsterRolodex/static/js/runtime-main.56421f5a.js"
+  }
+]);
